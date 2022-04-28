b97b26adc1dda48faa24fc8edccd6eaff7caaa52
Виталий Дмитриев <dmitriz_vitaliy@mail.ru>
2022-04-28 11:56:43 +0500

    Add info window and TextMeshPro

---
+++ Assets/Scripts/Tutorial/TutorialCheckpoint.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System;
+
+public class TutorialCheckpoint : MonoBehaviour
+{
+    [SerializeField] private string informationText;
+    [SerializeField] private InformationWindowHidingConditions informationWindowHidingCondition = InformationWindowHidingConditions.None;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        InformationWindow.instance?.Show(informationText, GetFuncCondition());
+        Destroy(gameObject);
+    }
+
+    private Func<bool> GetFuncCondition()
+    {
+        switch (informationWindowHidingCondition)
+        {
+            case InformationWindowHidingConditions.Press1:
+                return new Func<bool>(() => { return Input.GetKey(KeyCode.Alpha1); });
+            default: return null;
+        }
+    }
+}
