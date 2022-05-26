0f4abcb392e9ea0b67fc242fe695193c4a4016bd
s1adk0ewka <Egor.Gainetdinov@urfu.me>
2022-05-26 17:33:49 +0500

    New quartz
    Quartz now has 0.5 scale from default title, and its now randomly attaching to nearby tiles.

---
+++ Assets/Scripts/Specifications/For tiles/Quartz.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Quartz : MonoBehaviour
+{
+    private Vector3 initialPosition;
+    private readonly float distance = 1f;
+    private readonly float distanceToMove = 0.25f;
+    private readonly Vector3 scale = new Vector3(0.5f, 0.5f, 0.5f);
+
+    public Vector3 InitialPosition { get => initialPosition; set => initialPosition = value; }
+
+    void Awake()
+    {
+        transform.localScale -= scale;
+        InitialPosition = transform.position;
+        var colliders = new List<Collider2D>
+        {
+            Physics2D.OverlapPoint(new Vector2(transform.position.x + distance, transform.position.y)),
+            Physics2D.OverlapPoint(new Vector2(transform.position.x - distance, transform.position.y)),
+            Physics2D.OverlapPoint(new Vector2(transform.position.x, transform.position.y + distance)),
+            Physics2D.OverlapPoint(new Vector2(transform.position.x, transform.position.y - distance))
+        }
+        .Where(collaider => collaider && collaider.gameObject.layer == LayerMask.NameToLayer(ServiceInfo.GroundLayerName) &&
+                !(collaider.gameObject.GetComponent<AttachedTile>()) )
+        .ToList();
+        var random = new System.Random();
+        /*for (int i = colliders.Count - 1; i >= 1; i--)
+        {
+            int j = random.Next(i + 1);
+            var temp = colliders[j];
+            colliders[j] = colliders[i];
+            colliders[i] = temp;
+        }*/
+        /*foreach (var collaider in colliders) {
+            Debug.Log(collaider);
+        }*/
+
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            colliders[random.Next(colliders.Count)].transform.position,
+            distanceToMove);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
