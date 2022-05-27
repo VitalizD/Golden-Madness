56b9b210bc6c18ce4961cef842455fbd5d561136
Tiranomage <elisey12379@gmail.com>
2022-05-27 19:23:11 +0500

    finale 1

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -6,15 +6,18 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public Slider slider;
+    public Text hp;
 
     public void Start()
     {
         slider.maxValue = 100;
         slider.value = 100;
+        hp.text = 100 + "/100";
     }
     public void SetHealth(int health)
     {
         slider.value = health;
+        hp.text = health + "/100";
     }
 
 }
--- Assets/Scripts/UI/SanityBar.cs
+++ Assets/Scripts/UI/SanityBar.cs
@@ -6,14 +6,17 @@ using UnityEngine.UI;
 public class SanityBar : MonoBehaviour
 {
     public Slider slider;
+    public Text sp;
 
     public void Start()
     {
         slider.maxValue = 100f;
         slider.value = 100f;
+        sp.text = 100 + "/100";
     }
     public void SetSanity(float sanity)
     {
         slider.value = sanity;
+        sp.text = System.Math.Round(sanity) + "/100";
     }
 }
