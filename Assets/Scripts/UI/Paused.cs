3203cca954d50a91e912d056ea68c0b7ca083c1d
Вадим <54228342+VadimFalaleev@users.noreply.github.com>
2022-06-04 19:24:16 +0500

    pauseMenu

--- Assets/Scripts/UI/Paused.cs
+++ Assets/Scripts/UI/Paused.cs
@@ -5,57 +5,77 @@ using UnityEngine.SceneManagement;
 
 public class Paused : MonoBehaviour
 {
-    [SerializeField] GameObject pause;
-    [SerializeField] GameObject exitMenu;
-    [SerializeField] GameObject control;
+    public static bool gameIsPause = false;
+    public static bool gameIsControl = false;
 
-    void Start()
-    {
-        pause.SetActive(false);
-        exitMenu.SetActive(false);
-        control.SetActive(false);
-    }
+    [SerializeField] GameObject PauseMenu;
+    [SerializeField] GameObject control;
+    //[SerializeField] GameObject exitMenu;
+
+    //void Start()
+    //{
+    //    pause.SetActive(false);
+    //    exitMenu.SetActive(false);
+    //    control.SetActive(false);
+    //}
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            pause.SetActive(true);
-            Time.timeScale = 0;
+            if (gameIsPause)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+        while (gameIsControl)
+        {
+            Time.timeScale = 0f;
         }
     }
 
-    public void PauseOff()
+    public void Resume()
     {
-        pause.SetActive(false);
-        Time.timeScale = 1;
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPause = false;
+    }
+
+    void Pause()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPause = true;
     }
 
     public void Control()
     {
-        pause.SetActive(false);
         control.SetActive(true);
-        Time.timeScale = 0;
+        gameIsControl = true;
     }
 
-    public void Menu()
-    {
-        pause.SetActive(false);
-        exitMenu.SetActive(true);
-        Time.timeScale = 0;
-    }
+    //public void Menu()
+    //{
+    //    pause.SetActive(false);
+    //    exitMenu.SetActive(true);
+    //    Time.timeScale = 0;
+    //}
 
-    public void Return()
-    {
-        pause.SetActive(true);
-        exitMenu.SetActive(false);
-        control.SetActive(false);
-        Time.timeScale = 0;
-    }
+    //public void Return()
+    //{
+    //    pause.SetActive(true);
+    //    exitMenu.SetActive(false);
+    //    control.SetActive(false);
+    //    Time.timeScale = 0;
+    //}
 
-    public void Exit()
-    {
-        SceneManager.LoadScene(1);
-        Time.timeScale = 1;
-    }
+    //public void Exit()
+    //{
+    //    SceneManager.LoadScene(1);
+    //    Time.timeScale = 1;
+    //}
 }
--- Assets/Scripts/YandexSDK.cs
+++ Assets/Scripts/YandexSDK.cs
@@ -5,6 +5,10 @@ public class YandexSDK : MonoBehaviour
 {
     void Start()
     {
+        VideoAd.Show();
+    }
+    void Update()
+    {
         InterestialAd.Show();
     }
 }
