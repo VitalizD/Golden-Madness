a6c56af260cd9516b15fadae903827d6cd45f2d5
Tiranomage <elisey12379@gmail.com>
2022-06-06 22:55:48 +0500

    Finally fixed all bugs

--- Assets/Scripts/UI/LampBar.cs
+++ Assets/Scripts/UI/LampBar.cs
@@ -6,11 +6,16 @@ using UnityEngine.UI;
 public class LampBar : MonoBehaviour
 {
     public Slider slider;
-    [SerializeField] private Lamp Lamp;
+    [SerializeField] private Lamp Lamp; 
 
     public void Start()
     {
         slider.maxValue = 100f;
+        slider.value = Lamp.FuelCount;
+    }
+
+    public void Update()
+    {
         slider.value = Lamp.FuelCount;
     }
     public void SetLamp(float lamp)
