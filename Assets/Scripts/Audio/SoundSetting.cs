9b2f1514281e6c08ce537d08d6872e66061bd78d
s1adk0ewka <Egor.Gainetdinov@urfu.me>
2022-07-15 00:56:57 +0500

    Fixed issues after merge with main

--- Assets/Scripts/Audio/SoundSetting.cs
+++ Assets/Scripts/Audio/SoundSetting.cs
@@ -7,6 +7,7 @@ public class SoundSetting : MonoBehaviour
 {
     [SerializeField] private Slider gameVolume;
     public static SoundSetting Instanse;
+    private Camera camera;
     //public delegate void VolumeChange(float x);
     //public event VolumeChange onVolumeChanged;
 
@@ -20,9 +21,15 @@ public class SoundSetting : MonoBehaviour
             Destroy(gameObject);
         gameVolume.value = 0.01f;
     }
+    private void Start()
+    {
+        //CameraController.Instanse.TryGetComponent<Camera>(out camera);
+    }
 
     private void Update()
     {
+        
+        //if(camera!=null) gameVolume.gameObject.transform.position = camera.transform.position + new Vector3(0, 1, 0);
         gameVolume.gameObject.transform.position = Player.Instanse.transform.position+new Vector3(0,1,0);
         //gameVolume.onValueChanged.AddListener(x => onVolumeChanged?.Invoke(x));
 
--- Assets/Scripts/UI/SoundButton.cs
+++ Assets/Scripts/UI/SoundButton.cs
@@ -6,6 +6,13 @@ public class SoundButton : MonoBehaviour
     public AudioClip hoverFx;
     public AudioClip clickFx;
 
+    private void Start()
+    {
+        if (myFx != null)
+            myFx.volume = SFX.MasterVol;
+        SoundSetting.Instanse.GameVolume.onValueChanged.AddListener(value => ChangeVol(value));
+    }
+
     public void HoverSound()
     {
         myFx.PlayOneShot(hoverFx);
@@ -14,5 +21,10 @@ public class SoundButton : MonoBehaviour
     public void ClickSound()
     {
         myFx.PlayOneShot(clickFx);
+    }
+
+    public void ChangeVol(float value)
+    {
+        if (myFx != null) myFx.volume = value;
     }
 }
